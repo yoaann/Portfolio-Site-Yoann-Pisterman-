2e356d3d40fa5dfbe79cd40bc0d86b22e1fcde5a
yoaann <94788240+yoaann@users.noreply.github.com>
2022-01-08 18:20:05 +0100

    2.6 adding Note taking app page html & css

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 console.log("Welcome to my Portfolio site");
-// You can press the green "run" button to execute the lesson from above
-// Feel free to experiment with the code by changing some values here or "open in repl.it button" on the right
+
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
